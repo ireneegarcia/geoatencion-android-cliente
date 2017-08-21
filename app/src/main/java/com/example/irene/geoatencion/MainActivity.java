d25982b609b18b3fd927e93efc0e5a89d461fd91
ireneegarcia <irenegarcia103@gmail.com>
2017-08-21 13:38:43 -0400

    fragment profile

--- app/src/main/java/com/example/irene/geoatencion/MainActivity.java
+++ app/src/main/java/com/example/irene/geoatencion/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.irene.geoatencion;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,10 +12,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    //static Toolbar toolbar;
+    View mView;
+    Context c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -91,12 +93,22 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_profile) {
+            Fragment fragment = new ProfileFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment, fragment)
+                    .commit();
         } else if (id == R.id.nav_gallery) {
+
+
 
         } else if (id == R.id.nav_slideshow) {
 
+        }else if (id == R.id.nav_map) {
+            Fragment fragment = new MapsFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment, fragment)
+                    .commit();
         } /*else if (id == R.id.nav_log_out) {
             Intent intent = new Intent (this, LoginActivity.class);
             startActivityForResult(intent, 0);
---
+++ app/src/main/java/com/example/irene/geoatencion/ProfileFragment.java
@@ -0,0 +1,32 @@
+package com.example.irene.geoatencion;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ProfileFragment extends Fragment {
+
+    View mView;
+    Context c;
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_profile, container, false);
+        c = (Context)getActivity();
+
+        return mView;
+    }
+
+
+}
