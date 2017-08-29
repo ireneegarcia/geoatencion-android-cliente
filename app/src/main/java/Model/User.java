7d325b3f71f5dd207c1a3d7ff002c8c994cb47a5
ireneegarcia <irenegarcia103@gmail.com>
2017-08-28 22:25:47 -0400

    Se cambiaron los iconos del menu

--- app/src/main/java/Model/User.java
+++ app/src/main/java/Model/User.java
@@ -34,7 +34,7 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
+        return "user{" +
                 "id='" + id + '\'' +
                 ", displayName='" + displayName + '\'' +
                 '}';
